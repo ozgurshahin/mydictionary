806627942e65da09bb985cdee3db13cb3af3fed5
Ozgur Demir <ozgur.demir@freelancer.run>
2020-05-28 10:18:48 +0100

    Swagger Globalda Calsiti V1 + controller lari actim

--- src/main/java/com/dictionary/Integration/IntegrationController.java
+++ src/main/java/com/dictionary/Integration/IntegrationController.java
@@ -1,32 +1,32 @@
-//package com.dictionary.Integration;
-//
-//
-//import com.dictionary.common.IntegrationApi;
-//import com.dictionary.iam.CurrentPrinciple;
-//import com.dictionary.iam.Principle;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.validation.constraints.NotNull;
-//
-//@Api(tags = {"Integration"})
-//@RestController
-//@RequestMapping("/integration")
-//@RequiredArgsConstructor(onConstructor = @__({@Autowired, @NotNull}))
-//public class IntegrationController {
-//
-//    private final CurrentPrinciple currentPrinciple;
-//
-//    @GetMapping("/me")
-//    @IntegrationApi
-//    @ApiOperation(value = "Get current integration principal", notes = "Permission : INTEGRATION ")
-//    public ResponseEntity<Principle> me() {
-//        return ResponseEntity.ok(currentPrinciple.getPrinciple());
-//    }
-//}
+package com.dictionary.Integration;
+
+
+import com.dictionary.common.IntegrationApi;
+import com.dictionary.iam.CurrentPrinciple;
+import com.dictionary.iam.Principle;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+
+@Api(tags = {"Integration"})
+@RestController
+@RequestMapping("/integration")
+@RequiredArgsConstructor(onConstructor = @__({@Autowired, @NotNull}))
+public class IntegrationController {
+
+    private final CurrentPrinciple currentPrinciple;
+
+    @GetMapping("/me")
+    @IntegrationApi
+    @ApiOperation(value = "Get current integration principal", notes = "Permission : INTEGRATION ")
+    public ResponseEntity<Principle> me() {
+        return ResponseEntity.ok(currentPrinciple.getPrinciple());
+    }
+}
--- src/main/java/com/dictionary/admin/OperatorController.java
+++ src/main/java/com/dictionary/admin/OperatorController.java
@@ -1,32 +1,32 @@
-//package com.dictionary.admin;
-//
-//import com.dictionary.common.AdminApi;
-//import com.dictionary.iam.CurrentPrinciple;
-//import com.dictionary.iam.Principle;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.validation.constraints.NotNull;
-//
-//
-//@Api(tags = {"Operator"})
-//@RestController
-//@RequestMapping("/admin")
-//@RequiredArgsConstructor(onConstructor = @__({@Autowired, @NotNull}))
-//public class OperatorController {
-//
-//    private final CurrentPrinciple currentPrinciple;
-//
-//    @GetMapping("/me")
-//    @AdminApi
-//    @ApiOperation(value = "Get current admin principal", notes = "Permission : OPERATOR ")
-//    public ResponseEntity<Principle> me() {
-//        return ResponseEntity.ok(currentPrinciple.getPrinciple());
-//    }
-//}
+package com.dictionary.admin;
+
+import com.dictionary.common.AdminApi;
+import com.dictionary.iam.CurrentPrinciple;
+import com.dictionary.iam.Principle;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+
+
+@Api(tags = {"Operator"})
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor(onConstructor = @__({@Autowired, @NotNull}))
+public class OperatorController {
+
+    private final CurrentPrinciple currentPrinciple;
+
+    @GetMapping("/me")
+    @AdminApi
+    @ApiOperation(value = "Get current admin principal", notes = "Permission : OPERATOR ")
+    public ResponseEntity<Principle> me() {
+        return ResponseEntity.ok(currentPrinciple.getPrinciple());
+    }
+}
